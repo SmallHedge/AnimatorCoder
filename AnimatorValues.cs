820859afb5e844371ef2608896ef59405e863f94
Ryan <95608156+SmallHedge@users.noreply.github.com>
2024-04-27 09:56:46 +1200

    Update AnimatorValues.cs
    Enums easier to understand

--- AnimatorValues.cs
+++ AnimatorValues.cs
@@ -6,6 +6,7 @@ namespace SHG.AnimatorCoder
     /// <summary> Complete list of all animation state names </summary>
     public enum Animations
     {
+        //Change the list below to your animation state names
         IDLE,
         RUN,
         ATTACK1,
@@ -13,12 +14,13 @@ namespace SHG.AnimatorCoder
         HIT,
         JUMP,
         FALL,
-        RESET
+        RESET  //Keep Reset
     }
 
     /// <summary> Complete list of all animator parameters </summary>
     public enum Parameters
     {
+        //Change the list below to your animator parameters
         GROUNDED,
         FALLING
     }
